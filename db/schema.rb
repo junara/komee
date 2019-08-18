0c563f8b6f95ccef1190d2122726d3fb1b9ec4c1
junara <jun5araki@gmail.com>
2019-08-18 21:01:02 +0900

    upgrade rails 6 and yarn upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 2019_01_26_135823) do
   create_table "items", force: :cascade do |t|
     t.string "message"
     t.string "token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
